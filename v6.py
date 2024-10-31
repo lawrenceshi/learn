58125359b29d5d651ab20b5382454010df3c74f4
Hao Shi <boruoshihao@gmail.com>
2024-10-31 09:16:07 -0400

    add a coin flip function.

---
+++ v6.py
@@ -0,0 +1,13 @@
+from random import randint
+
+def flip_coin_raitio(N):
+
+    front = 0
+
+    for i in range(N):
+        randomint_num = randint(1, 2)
+        if randomint_num == 1:
+            front+=1
+
+    return front*1.0/N
+    
