72c705f57cf9cd87232cf1d6829681d0bf100d64
lawrrenceshi <shilaw5201@139.com>
2024-12-13 21:55:55 +0800

    开了个赌场的游戏，玩一次1元，如果抛硬币到正面赢1.8，抛到反面啥都得不到，最终结果是总是亏钱

---
+++ v13.py
@@ -0,0 +1,36 @@
+#大数定理
+from random import randint
+import numpy as np
+import matplotlib.pyplot as plt
+#本金
+
+bj=int(input('本金:'))
+#bj=10000
+
+def flip_coin_raitio(N):
+
+    front = 0
+    back = 0
+
+    for i in range(N):
+        randomint_num = randint(1, 2)
+        if randomint_num == 1:
+            front+=1
+            #print('本金+1.8')
+        elif randomint_num == 2:
+            back+=1
+            #print('本金-1')
+    print('正面:'+str(front))
+    print('反面:'+str(back))
+    return [front*0.8,back*1.0]
+
+#ydq=赢的钱,sdq=输的钱
+ydq,sdq= flip_coin_raitio(bj)
+
+if ydq > sdq:
+    print('盈利:',ydq-sdq)
+else:
+    print('亏损:',sdq-ydq)
+
+print(ydq)
+print(sdq)
